eb5db82e5675c3daf4eb5f9255dd26548adef40a
Jarred Capellman <jcapellman@hotmail.com>
2017-06-27 20:22:21 -0500

    Added: PlayerNames Table Class

---
+++ src/shootarama/DB/Tables/PlayerNames.cs
@@ -0,0 +1,9 @@
+﻿namespace shootarama.DB.Tables
+{
+    public class PlayerNames : BaseTable
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
