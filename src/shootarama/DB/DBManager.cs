dfe596b713b244df4cffdcc72eb3c22d428d2333
Jarred Capellman <jcapellman@hotmail.com>
2017-06-27 19:34:42 -0500

    Added: DBManager Class

---
+++ src/shootarama/DB/DBManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using shootarama.DB.Tables;
+
+namespace shootarama.DB
+{
+    public class DBManager : DbContext
+    {
+        public DbSet<Players> Players { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={Common.Constants.FILENAME_SQLITE_DB}");
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            var changeSet = ChangeTracker.Entries();
+
+            if (changeSet == null)
+            {
+                return base.SaveChangesAsync(cancellationToken);
+            }
+
+            foreach (var entry in changeSet.Where(c => c.State != EntityState.Unchanged))
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Member("Created").CurrentValue = DateTime.Now;
+                        entry.Member("Active").CurrentValue = true;
+                        break;
+                }
+
+                entry.Member("Modified").CurrentValue = DateTime.Now;
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
