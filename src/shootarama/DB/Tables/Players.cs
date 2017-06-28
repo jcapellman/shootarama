ce65ff067089da4fa57c912d9a0aa0b2563dba7f
Jarred Capellman <jcapellman@hotmail.com>
2017-06-27 19:35:11 -0500

    Added: Players Table Class

---
+++ src/shootarama/DB/Tables/Players.cs
@@ -0,0 +1,23 @@
+﻿namespace shootarama.DB.Tables
+{
+    public class Players : BaseTable
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public int TeamID { get; set; }
+
+        public int Experience { get; set; }
+
+        public int Offense { get; set; }
+
+        public int Defense { get; set; }
+
+        public int Clutch { get; set; }
+
+        public int Salary { get; set; }
+    }
+}
