f2fc9560a26c18953a91b341b52922b367e1cade
Jarred Capellman <jcapellman@hotmail.com>
2017-06-28 21:29:51 -0500

    Added: Team Count and Player Count Constants

--- src/shootarama/Common/Constants.cs
+++ src/shootarama/Common/Constants.cs
@@ -3,5 +3,11 @@
     public static class Constants
     {
         public const string FILENAME_SQLITE_DB = "game.db";
+
+        public const int NUMBER_OF_TEAMS = 32;
+
+        public const int NUMBER_OF_PLAYERS_PER_TEAM = 5;
+
+        public const double PLAYER_CAP = 250.0;
     }
 }
