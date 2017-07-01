f5f052ca2c29f2fee67b837a35dfd0d519ea57ce
Jarred Capellman <jcapellman@hotmail.com>
2017-06-30 21:03:52 -0500

    Change:  Refactoring the BaseGameType class

--- src/shootarama/GameTypes/BaseGameType.cs
+++ src/shootarama/GameTypes/BaseGameType.cs
@@ -1,9 +1,64 @@
-﻿namespace shootarama.GameTypes
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using shootarama.DB;
+using shootarama.DB.Tables;
+
+namespace shootarama.GameTypes
 {
     public abstract class BaseGameType
     {
+        private DBManager _dbManager;
+
         protected int GameID;
 
-        public abstract void CreateNewGame(string teamName);
+        protected List<Teams> CreateTeams(string teamName)
+        {
+            using (var db = new DBManager())
+            {
+                var teams = new List<Teams>();
+
+                var random = new Random((int)DateTime.Now.Ticks);
+
+                var teamNames = db.TeamNames.OrderBy(a => random.Next()).Take(Common.Constants.NUMBER_OF_TEAMS - 1).Select(a => a.Name).ToList();
+                var locationNames = db.LocationNames.OrderBy(a => random.Next())
+                    .Take(Common.Constants.NUMBER_OF_TEAMS - 1).Select(a => a.Name).ToList();
+
+                for (int x = 0; x < Common.Constants.NUMBER_OF_TEAMS - 1; x++)
+                {
+                    var team = new Teams
+                    {
+                        GameID = this.GameID,
+                        Name = teamNames[x],
+                        Location = locationNames[x]
+                    };
+
+                    teams.Add(team);
+                    db.SaveChanges();
+                }
+
+                return teams;
+            }
+        }
+
+        protected void GenerateGame(string firstName, string lastName)
+        {
+            using (var db = new DBManager())
+            {
+                var game = new Games
+                {
+                    FirstName = firstName,
+                    LastName = lastName
+                };
+
+                db.Games.Add(game);
+                db.SaveChanges();
+
+                GameID = game.ID;
+            }
+        }
+
+        public abstract void CreateNewGame(string firstName, string lastName, string teamName);
     }
 }
--- src/shootarama/GameTypes/MyTeam.cs
+++ src/shootarama/GameTypes/MyTeam.cs
@@ -1,46 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using shootarama.DB;
-using shootarama.DB.Tables;
-
-namespace shootarama.GameTypes
+﻿namespace shootarama.GameTypes
 {
     public class MyTeam : BaseGameType
     {
-        private DBManager _dbManager;
-
-        private List<Teams> CreateTeams(string teamName)
+        public override void CreateNewGame(string firstName, string lastName, string teamName)
         {
-            using (var db = new DBManager())
-            {
-                var teams = new List<Teams>();
+            GenerateGame(firstName, lastName);
 
-                var random = new Random((int) DateTime.Now.Ticks);
-
-                var teamNames = db.TeamNames.OrderBy(a => random.Next()).Take(Common.Constants.NUMBER_OF_TEAMS - 1).Select(a => a.Name).ToList();
-                var locationNames = db.LocationNames.OrderBy(a => random.Next())
-                    .Take(Common.Constants.NUMBER_OF_TEAMS - 1).Select(a => a.Name).ToList();
-
-                for (int x = 0; x < Common.Constants.NUMBER_OF_TEAMS - 1; x++)
-                {
-                    var team = new Teams
-                    {
-                        GameID = this.GameID,
-                        Name = teamNames[x],
-                        Location = locationNames[x]
-                    };
-
-                    teams.Add(team);
-                }
-
-                return teams;
-            }
-        }
-
-        public override void CreateNewGame(string teamName)
-        {
+            var teams = CreateTeams(teamName);
         }
     }
 }
